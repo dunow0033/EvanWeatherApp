bda1b353c3f91a02fb86b47044f64b353a63b452
Brian Donnoe <dunow33@hotmail.com>
2022-07-10 01:43:10 -0400

    needs more work

---
+++ app/src/main/java/com/express/android/evanweatherapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.express.android.evanweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    val CITY: String = "dhaka,bd"
+    val API:
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
